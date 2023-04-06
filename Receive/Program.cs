6588403d2137daa26bc1d2dd1701f8913e70b495
GabrielVictor159 <gabrielvictor159487@gmail.com>
2023-04-06 11:59:37 -0300

    projeto RabbitMQ simples

---
+++ Receive/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" }; // Altere o nome do host se necessário
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "fila_teste", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Mensagem recebida: {0}", message);
+            };
+            channel.BasicConsume(queue: "fila_teste", autoAck: true, consumer: consumer);
+
+            Console.WriteLine("Pressione qualquer tecla para sair.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" }; // Altere o nome do host se necessário
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "fila_teste", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            string message = "Olá, RabbitMQ!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "", routingKey: "fila_teste", basicProperties: null, body: body);
+            Console.WriteLine("Mensagem enviada: {0}", message);
+        }
+
+        Console.WriteLine("Pressione qualquer tecla para sair.");
+        Console.ReadKey();
+    }
+}
